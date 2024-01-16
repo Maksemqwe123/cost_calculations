5a1a0d1e0fb71bab2722a22c789882dbbfda395a
Maksemqwe123 <Makcemjgffjh4@gmail.com>
2024-01-17 01:10:11 +0300

    Adding user interaction with the login form

--- expense_tracker_website/main/views.py
+++ expense_tracker_website/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 
@@ -8,5 +9,13 @@ def index(request):
     return HttpResponse("<h1>Main Page</h1>")
 
 
+@csrf_protect
 def sign_in_user(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    context = {
+        'css_path': 'css/styles.css'
+    }
+    login = request.POST['login']
+    password = request.POST['password']
+    print(login)
+    print(password)
+    return render(request, 'form_sign_in.html', context)
